1dec96db1efd73ce8d84d99923c7b75f2b387d4d
Daniel Marton <daniel.marton91@gmail.com>
2025-11-03 10:18:57 +0100

    Update .gitignore

--- src/main/java/com/example/HelloFX.java
+++ src/main/java/com/example/HelloFX.java
@@ -11,6 +11,8 @@ public class HelloFX extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+        System.out.println(System.getenv("HOST_NAME"));
+
         FXMLLoader fxmlLoader = new FXMLLoader(HelloFX.class.getResource("hello-view.fxml"));
         Parent root = fxmlLoader.load();
         Scene scene = new Scene(root, 640, 480);
