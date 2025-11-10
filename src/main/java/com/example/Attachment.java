3c36e3cbaa3fe7220c5849a8a09b3e48d42cd95b
Daniel Marton <daniel.marton91@gmail.com>
2025-11-10 22:08:19 +0100

    Cleaned up code and added comments

--- src/main/java/com/example/Attachment.java
+++ src/main/java/com/example/Attachment.java
@@ -2,5 +2,13 @@ package com.example;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+/**
+ * Used for parsing attachment json into Dto
+ * @param name
+ * @param type
+ * @param size
+ * @param expires
+ * @param url
+ */
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record Attachment(String name, String type, long size, long expires, String url){}
+public record AttachmentDto(String name, String type, long size, long expires, String url){}
